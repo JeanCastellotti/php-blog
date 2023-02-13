915a275bfc9d8f8098342f406b96458369d69b87
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-02-13 12:45:59 +0100

    Configuration improvements

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
+// require 'router.php';
 
-require 'router.php';
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+$posts = $db->query('SELECT * FROM posts')->fetchAll();
