a031dd7e09e1478889294c02c842454f02fc1148
JeanDevFR <castellotti.j@gmail.com>
2023-02-17 13:50:29 +0100

    build a better router

--- core/router.php
+++ core/router.php
@@ -1,24 +1,72 @@
 <?php
 
-$routes = require base_path('routes.php');
+namespace Core;
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-function abort($statusCode = 404)
+class Router
 {
-    http_response_code($statusCode);
-    require base_path("views/{$statusCode}.php");
-    die();
-}
+    protected $routes;
 
-function routeToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require base_path($routes[$uri]);
+    public function get($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'GET'
+        ];
     }
-    else {
-        abort();
+
+    public function post($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'POST'
+        ];
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'DELETE'
+        ];
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PUT'
+        ];
+    }
+
+    public function patch($uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PATCH'
+        ];
+    }
+
+    public function route($uri)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri) {
+                return require base_path($route['controller']);
+            }
+        }
+
+        $this->abort();
+    }
+
+    protected function abort($statusCode = 404) {
+        http_response_code($statusCode);
+
+        require base_path("views/{$statusCode}.php");
+        
+        die();
     }
 }
-
-routeToController($uri, $routes);
--- routes.php
+++ routes.php
@@ -1,10 +1,12 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/posts' => 'controllers/posts/index.php',
-    '/post' => 'controllers/posts/show.php',
-    '/posts/create' => 'controllers/posts/create.php'
-];
+// return [
+//     '/' => 'controllers/index.php',
+//     '/about' => 'controllers/about.php',
+//     '/contact' => 'controllers/contact.php',
+//     '/posts' => 'controllers/posts/index.php',
+//     '/post' => 'controllers/posts/show.php',
+//     '/posts/create' => 'controllers/posts/create.php'
+// ];
+
+$router->get('/', 'controllers/index.php');
